31ae2a41d47a78b0948f346aea1ebba825eb9ae6
Ali <qali372@gmail.com>
2024-02-06 14:45:47 +0200

    feat : add 4 routes for doctor-apis.php (login - register - doctor - logout)

--- routes/doctor-apis.php
+++ routes/doctor-apis.php
@@ -1,1 +1,19 @@
 <?php
+
+use App\Http\Controllers\Api\Doctors\DoctorAuthController;
+use Illuminate\Support\Facades\Route;
+
+// User Routes
+Route::group(['prefix' => 'doctors'], function () {
+
+    // Auth Routes
+    Route::controller(DoctorAuthController::class)->group(function () {
+        Route::post('login', 'login');
+        Route::post('register', 'register');
+
+        Route::middleware('auth.doctor')->group(function () {
+            Route::get('doctor', 'currentDoctor');
+            Route::get('logout', 'logout');
+        });
+    });
+});
