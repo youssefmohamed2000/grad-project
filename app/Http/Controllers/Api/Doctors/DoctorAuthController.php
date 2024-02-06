9b7936d2a44f1ebac68fd90ab00e7450bce9b227
Ali <qali372@gmail.com>
2024-02-06 15:01:42 +0200

    build : DoctorAuthController.php with login method

---
+++ app/Http/Controllers/Api/Doctors/DoctorAuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api\Doctors;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Doctors\DoctorLoginRequest;
+use App\Http\Resources\DoctorResource;
+use App\Traits\ResponseTrait;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DoctorAuthController extends Controller
+{
+    use ResponseTrait;
+
+    public function login(DoctorLoginRequest $request): JsonResponse
+    {
+        $email = $request->validated('email');
+        $password = $request->validated('password');
+
+        if (!Auth::attempt(['email' => $email, 'password' => $password])) {
+            return $this->sendError('Auth failed', 'this credentials don\'t match our records');
+        }
+
+        $doctor = Auth::guard('doctor')->user();
+        $token = $doctor->createToken('doctor-access-token')->plainTextToken;
+
+        $data = [
+            'user' => new DoctorResource($doctor),
+            'token' => $token
+        ];
+
+        return $this->sendResponse($data, 'User Logged in Successfully');
+    }
+
+
+}
