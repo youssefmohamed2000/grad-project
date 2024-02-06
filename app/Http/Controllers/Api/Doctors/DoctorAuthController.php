283c32e20d79aa68ff8a24d51965d3e58f246ac2
Ali <qali372@gmail.com>
2024-02-06 15:14:01 +0200

    feat : add register method to DoctorAuthController.php

--- app/Http/Controllers/Api/Doctors/DoctorAuthController.php
+++ app/Http/Controllers/Api/Doctors/DoctorAuthController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api\Doctors;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Doctors\DoctorLoginRequest;
+use App\Http\Requests\Api\Doctors\DoctorRegisterRequest;
 use App\Http\Resources\DoctorResource;
+use App\Models\Doctor;
 use App\Traits\ResponseTrait;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -34,5 +36,21 @@ class DoctorAuthController extends Controller
         return $this->sendResponse($data, 'User Logged in Successfully');
     }
 
+    public function register(DoctorRegisterRequest $request): JsonResponse
+    {
+        $credentials = $request->safe()->merge([
+            'password' => bcrypt($request->validated('password')),
+        ]);
+
+        $doctor = Doctor::create($credentials);
+        $token = $doctor->createToken('doctor-access-token')->plainTextToken;
+
+        $data = [
+            'user' => new DoctorResource($doctor),
+            'token' => $token
+        ];
+
+        return $this->sendResponse($data, 'User Registered Successfully');
+    }
 
 }
