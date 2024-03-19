9a64d843d28fe2626b21405219569dfc19d23126
Ali <qali372@gmail.com>
2024-03-20 01:02:04 +0200

    build : OperationFactory.php

---
+++ database/factories/OperationFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Doctor;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Operation>
+ */
+class OperationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'type' => $this->faker->text,
+            'date' => $this->faker->date,
+            'effect' => $this->faker->text,
+            'doctor_name' => Doctor::all()->random()->name,
+        ];
+    }
+}
