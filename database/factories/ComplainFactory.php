a65eeef163d2f929e2dc3b889bc0e67b68001a53
Ali <qali372@gmail.com>
2024-03-19 22:10:11 +0200

    build : ComplainFactory.php

---
+++ database/factories/ComplainFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Complain>
+ */
+class ComplainFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'complain' => $this->faker->text,
+            'start_date' => $this->faker->date,
+            'increase_with' => $this->faker->text,
+            'decrease_with' => $this->faker->text,
+        ];
+    }
+}
