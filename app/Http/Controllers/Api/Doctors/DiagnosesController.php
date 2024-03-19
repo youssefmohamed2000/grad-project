426b9d4bce599c6998dc5b816f2ed23cf70c9d6d
Ali <qali372@gmail.com>
2024-03-20 00:23:17 +0200

    build : DiagnosesController.php

---
+++ app/Http/Controllers/Api/Doctors/DiagnosesController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Api\Doctors;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Doctors\DiagnoseStoreRequest;
+use App\Http\Requests\Api\Doctors\DiagnoseUpdateRequest;
+use App\Http\Resources\DiagnoseResource;
+use App\Models\Diagnose;
+use App\Traits\Helper;
+use Illuminate\Http\JsonResponse;
+
+class DiagnosesController extends Controller
+{
+    use Helper;
+
+    public function __construct()
+    {
+        $this->middleware('permission:read_diagnoses,doctor')->only('index', 'show');
+        $this->middleware('permission:create_diagnoses,doctor')->only('store');
+        $this->middleware('permission:update_diagnoses,doctor')->only('update');
+        $this->middleware('permission:delete_diagnoses,doctor')->only('delete');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index() : JsonResponse
+    {
+        $diagnoses = Diagnose::paginate();
+
+        $paginationData = $this->getPaginationData($diagnoses);
+
+        return $this->sendResponse(
+            DiagnoseResource::collection($diagnoses),
+            'diagnoses sent successfully',
+            $paginationData
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(DiagnoseStoreRequest $request) : JsonResponse
+    {
+        $diagnose = Diagnose::create($request->validated());
+
+        return $this->sendResponse(
+            new DiagnoseResource($diagnose),
+            'diagnose created successfully'
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id) : JsonResponse
+    {
+        $diagnose = Diagnose::find($id);
+        if (!$diagnose){
+            return $this->sendError('diagnose not found');
+        }
+
+        return $this->sendResponse(new DiagnoseResource($diagnose), 'diagnose sent successfully');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(DiagnoseUpdateRequest $request, string $id) : JsonResponse
+    {
+        $diagnose = Diagnose::find($id);
+        if (!$diagnose){
+            return $this->sendError('diagnose not found');
+        }
+
+        $diagnose->update($request->validated());
+
+        return $this->sendResponse(new DiagnoseResource($diagnose), 'diagnose updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id) : JsonResponse
+    {
+        $diagnose = Diagnose::find($id);
+        if (!$diagnose){
+            return $this->sendError('diagnose not found');
+        }
+
+        $diagnose->delete();
+        return $this->sendResponse(new DiagnoseResource($diagnose), 'diagnose deleted successfully');
+    }
+}
