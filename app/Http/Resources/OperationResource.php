b6481a6bebc32241b22efe7b1544723e87fcceca
Ali <qali372@gmail.com>
2024-03-20 01:02:12 +0200

    build : OperationResource.php

---
+++ app/Http/Resources/OperationResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OperationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user' => new UserResource($this->user),
+            'type' => $this->type,
+            'date' => $this->date,
+            'effect' => $this->effect,
+            'doctor_name' => $this->doctor_name,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
