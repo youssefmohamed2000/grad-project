09739512bb4ecfb330066b1babd7fd95c14f9eee
Ali <qali372@gmail.com>
2024-03-20 00:22:34 +0200

    build : Diagnose Store and Update Requests

---
+++ app/Http/Requests/Api/Doctors/DiagnoseUpdateRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Api\Doctors;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DiagnoseUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'complain_id' => 'sometimes|required|exists:complains,id',
+            'doctor_id' => 'sometimes|required|exists:doctors,id',
+            'diagnose' => 'sometimes|required',
+        ];
+    }
+}
